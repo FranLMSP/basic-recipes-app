cdb0098048f65076b12f21720b22ae43c590bf62
Franco Colmenarez <francoacg1@gmail.com>
2021-02-03 13:55:58 -0500

    Add fonts and theming

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeal',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            color: const Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyText2: const TextStyle(
+            color: const Color.fromRGBO(20, 51, 51, 1),
+          ),
+          headline6: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: CategoriesScreen(),
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -12,7 +12,10 @@ class CategoryItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(15),
-      child: Text(category.title),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.headline6
+      ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
         gradient: LinearGradient(
